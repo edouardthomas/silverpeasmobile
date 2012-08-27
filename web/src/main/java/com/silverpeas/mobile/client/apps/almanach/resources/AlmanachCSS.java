4c1ac4fd90159aa2c820e755d804b19672ed6b81
Edouard THOMAS <edouard.thomas@silverpeas.com>
2012-08-27 17:13:31 +0200

    maj contacts + almanach

--- web/src/main/java/com/silverpeas/mobile/client/apps/almanach/resources/AlmanachCSS.java
+++
@@ -1,8 +0,0 @@
-package com.silverpeas.mobile.client.apps.almanach.resources;
-
-import com.google.gwt.resources.client.CssResource;
-
-public interface AlmanachCSS extends CssResource{
-	String almanachs();
-	String cal();
-}
